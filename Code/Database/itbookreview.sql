16f0aafbc3214f3012b2a6f47b41b7b9965a5bf0
Anders <itbookreview@163.com>
2013-07-21 22:53:34 +0800

    先弄这8张表
    先把这些表的功能实现了再做别的表吧= =

--- Code/Database/itbookreview.sql
+++ Code/Database/itbookreview.sql
@@ -19,6 +19,9 @@ QQ varchar(20),
 portrait varchar(255) comment '头像',
 gender tinyint(1) not null comment '性别1：男，2：女,0:未知',
 birth date,
+job varchar(20),
+province varchar(40),
+city varchar(40),
 resume varchar(512) comment '个人简介',
 role tinyint(1) not null comment '用户角色',
 score int unsigned not null comment '用户积分，利用百度知道的那套积分系统',
@@ -32,10 +35,119 @@ primary key (id)
 
 create unique index idx_user_email on ibr_users ( email );
 create unique index idx_user_name on ibr_users ( name );
+/*==============================================================*/
+/* Table:ibr_book_catalogs 书籍的分类 */
+/*==============================================================*/
+drop table if exists ibr_book_catalogs;
+create table ibr_book_catalogs
+(
+id int unsigned not null auto_increment,
+name varchar(32) not null comment '分类名称',
+ident varchar(50) not null comment '分类英文名称',
+sort_order smallint not null comment '排序权值',
+primary key (id)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*==============================================================*/
+/* Table:ibr_books IT书籍 */
+/*==============================================================*/
+drop table if exists ibr_books;
+create table ibr_books
+(
+id int unsigned not null auto_increment,
+name varchar(128) not null comment '书名',
+ident varchar(128) comment '书名english',
+pic varchar(255) comment '图书封面',
+content text comment '内容简介',
+author varchar(128) not null comment '作者',
+translator varchar(128) not null comment '译者',
+publisher varchar(128) not null comment '出版社',
+publication_year date not null comment '出版时间',
+user_id int unsigned not null comment '上传书的贡献者',
+pages_number mediumint unsigned not null comment '页数',
+catalog_id int unsigned not null comment '书的分类',
+price double unsigned not null comment '定价',
+tags varchar(100) comment '标签',
+status tinyint(1) not null comment '书的状态0:未审核,1:ok,2:不OK',
+ctime timestamp not null default CURRENT_TIMESTAMP comment '创建时间',
+view_times mediumint not null default 0 comment '点击次数',
+comment_times mediumint not null default 0 comment '评论次数',
+note_times mediumint not null default 0 comment '笔记次数',
+as_top tinyint(1) not null default 0,
+primary key (id)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+create index idx_book_user on ibr_books ( user_id );
+create index idx_book_catalog on ibr_books ( catalog_id );
+
+/*==============================================================*/
+/* Table:ibr_tags 标签 */
+/*==============================================================*/
+drop table if exists ibr_tags;
+create table ibr_tags
+(
+id int unsigned not null auto_increment,
+name varchar(32) not null,
+relevant_count int unsigned not null default 0 comment '这个tag关联的book数目',
+primary key (id)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+create index idx_tag_name on ibr_tags ( name );
+
+/*==============================================================*/
+/* Table:ibr_book_comments 书评 */
+/*==============================================================*/
+drop table if exists ibr_book_comments;
+create table ibr_book_comments
+(
+id int unsigned not null auto_increment,
+content text comment '内容',
+user_id int unsigned not null comment '评论人',
+book_id int unsigned not null comment '针对哪本book',
+ctime timestamp not null default CURRENT_TIMESTAMP comment '创建时间',
+useful int not null default 0 comment '有多少人评价是有用的',
+useless int not null default 0 comment '有多少人评价是没用的',
+score int not null comment '对书的评价得分',
+primary key (id)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+create index idx_b_c_bookid on ibr_book_comments ( book_id );
+
+/*==============================================================*/
+/* Table:ibr_comment_votes 对书评投票的记录，不允许多次投票 */
+/*==============================================================*/
+drop table if exists ibr_comment_votes;
+create table ibr_comment_votes
+(
+id int unsigned not null auto_increment,
+vote_type tinyint not null default 0 comment '投的什么票',
+comment_id int unsigned not null comment '对哪个评论投的票',
+user_id int unsigned not null comment '投票人',
+primary key (id)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+create index idx_c_v_user_id on ibr_comment_votes ( user_id );
+
+/*==============================================================*/
+/* Table:ibr_book_comments 对书评的回应 */
+/*==============================================================*/
+drop table if exists ibr_comment_responses;
+create table ibr_comment_responses
+(
+id int unsigned not null auto_increment,
+content text comment '内容',
+user_id int unsigned not null comment '回应人',
+book_id int unsigned not null comment '针对哪个书评',
+ctime timestamp not null default CURRENT_TIMESTAMP comment '创建时间',
+primary key (id)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+create index idx_c_r_bookid on ibr_comment_responses ( book_id );
 
 /*==============================================================*/
 /* Table:ibr_links 友情链接 */
 /*==============================================================*/
+drop table if exists ibr_links;
 create table ibr_links
 (
 id int unsigned not null auto_increment,
