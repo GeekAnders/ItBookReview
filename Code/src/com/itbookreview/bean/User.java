9874814cfe0ee80da0502853bc264983e22e31c7
Anders <itbookreview@163.com>
2013-07-28 22:31:44 +0800

    做完注册几个页面了，还不错

--- Code/src/com/itbookreview/bean/User.java
+++ Code/src/com/itbookreview/bean/User.java
@@ -1,11 +1,33 @@
 package com.itbookreview.bean;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
 import my.mvc.IUser;
 
 public class User implements IUser{
 	public static final User INSTANCE = new User();
+	
+	private String email;
+	private String name;
+	private String pwd;
+	private String weibo;
+	private String QQ;
+	private String portrait;
+	private byte gender;
+	private Date birth;
+	private String job;
+	private String province;
+	private String city;
+	private String resume;
+	private byte role;
+	private int score;
+	private byte rank = 1;
+	private byte status;
+	private Timestamp reg_time;
+	
 
 	public String CacheRegion() {
 		// TODO Auto-generated method stub
